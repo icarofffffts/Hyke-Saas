df7282396e3d31fc71498b14be3e3e88b9cc8c45
Icaro Silva <ijv00777@gmail.com>
2026-03-11 19:58:32 -0300

    Consolidate dashboard to root and move temp to legacy

--- dashboard/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-import { defineConfig } from '@prisma/config'
-
-export default defineConfig({
-    datasource: {
-        url: process.env.DATABASE_URL,
-    },
-})
